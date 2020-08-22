20b809f110215aad124b0b537e6a4adf769037cf
Axarva <timsinaatharva@gmail.com>
2020-08-22 16:21:44 +0545

    Improve palindrome tester

--- bundle.js
+++ bundle.js
@@ -42,7 +42,27 @@ function Phrase(content){
 },{}],2:[function(require,module,exports){
 let Phrase = require("axarva-palindrome");
 
+function palindromeTester(event) {
+    event.preventDefault();
+    let string = event.target.phrase.value;
+    let phrase = new Phrase(string);
+    let palindromeResult = document.querySelector("#palindromeResult");
 
-let string = prompt("Please input something for a palindrome testing:")
-alert(new Phrase(string).palindrome());
+    if (phrase.palindrome() == `"${phrase.content}" be palindrome.`) {
+        palindromeResult.innerHTML = `"<strong>${phrase.content}</strong>" be a palindrome.`;
+      } else if(phrase.palindrome() == `"${phrase.content}" be no palindrome, pleb.`){
+        palindromeResult.innerHTML = `"<strong>${phrase.content}</strong>" be no palindrome, pleb.`;
+      } else {
+          palindromeResult.innerHTML = phrase.palindrome();
+      }
+    }
+document.addEventListener("DOMContentLoaded", function() {
+    let form = document.querySelector("#palindromeTester");
+    form.addEventListener("submit", function() {
+        palindromeTester(event);
+    });
+});
+
+
+
 },{"axarva-palindrome":1}]},{},[2]);
--- main.js
+++ main.js
@@ -1,5 +1,24 @@
 let Phrase = require("axarva-palindrome");
 
+function palindromeTester(event) {
+    event.preventDefault();
+    let string = event.target.phrase.value;
+    let phrase = new Phrase(string);
+    let palindromeResult = document.querySelector("#palindromeResult");
 
-let string = prompt("Please input something for a palindrome testing:")
-alert(new Phrase(string).palindrome());
+    if (phrase.palindrome() == `"${phrase.content}" be palindrome.`) {
+        palindromeResult.innerHTML = `"<strong>${phrase.content}</strong>" be a palindrome.`;
+      } else if(phrase.palindrome() == `"${phrase.content}" be no palindrome, pleb.`){
+        palindromeResult.innerHTML = `"<strong>${phrase.content}</strong>" be no palindrome, pleb.`;
+      } else {
+          palindromeResult.innerHTML = phrase.palindrome();
+      }
+    }
+document.addEventListener("DOMContentLoaded", function() {
+    let form = document.querySelector("#palindromeTester");
+    form.addEventListener("submit", function() {
+        palindromeTester(event);
+    });
+});
+
+
